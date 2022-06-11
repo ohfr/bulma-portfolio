571f83d507c0490776b05ca307a7670533ba46f1
Daniel <danielmartin1205@gmail.com>
2022-06-11 01:16:15 -0400

    updated node-sass and bulma

--- src/Components/About.js
+++ src/Components/About.js
@@ -17,33 +17,28 @@ const About = () => {
                             <div className="column">
                                 <h2 className="title is-size-4 is-spaced">Front-End</h2>
                                 <p className="list-title has-text-purple has-text-weight-bold">Languages</p>
-                                <p>JavaScript, HTML, CSS</p>
+                                <p>JavaScript, TypeScript, HTML, CSS</p>
                                 <p className="list-title has-text-purple has-text-weight-bold">Technologies</p>
                                 <ul>
                                     <li>React.js</li>
                                     <li>React Native</li>
                                     <li>Redux</li>
                                     <li>Jest</li>
-                                    <li>React-Router</li>
-                                    <li>Bulma</li>
-                                    <li>Material-UI</li>
-                                    <li>Bootstrap</li>
-                                    <li>LESS</li>
-                                    <li>SASS</li>
-                                    <li>Axios</li>
                                 </ul>
                             </div>
                             <div className="column">
                                 <h2 className="title is-size-4 is-spaced">Back-End</h2>
                                 <p className="list-title has-text-purple has-text-weight-bold">Languages</p>
-                                <p>SQL, JavaScript, Java, Python</p>
+                                <p>SQL, JavaScript, TypeScript, Java, Python</p>
                                 <p className="list-title has-text-purple has-text-weight-bold">Technologies</p>
                                 <ul>
+                                    <li>AWS</li>
                                     <li>Django</li>
+                                    <li>MongoDB</li>
                                     <li>Node.js</li>
-                                    <li>Knex.js</li>
                                     <li>SQLite3</li>
                                     <li>PostgreSQL</li>
+                                    <li>DynamoDB</li>
                                 </ul>
                             </div>
                         </div>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -11,16 +11,15 @@ const Home = () => {
                 <div className="hero-body">
                     <div className="container">
                         <h1 className="title flex">Hi there, I'm <Typical className= "title purple" steps={['Daniel', 2000, 'a developer', 2000, 'a keyboard addict', 2000, 'a car enthusiast', 2000]} loop={Infinity}/></h1>
-                        <h2 className="subtitle">I build computers, keyboards, and software.</h2>
+                        <h2 className="subtitle">I build cars, keyboards, and software.</h2>
                         <p className="subtitle">
-                            I'm a software engineer from Vinton, VA. I'm an enthusiastic programmer who loves to learn anything new! I mainly work with Django and React, but I'm never afraid to learn a new language or framework. I graduated from Lambda School in September 2020, with a focus in web development, Lambda taught me to push myself and to always be hungry for new knowledge.
+                            I'm a software engineer from Vinton, VA. I'm an enthusiastic programmer who loves to learn anything new! I currently work at Genesys as a Software Engineer, I work on a realtime chat app using TypeScript. I graduated from Lambda School in September 2020, with a focus in web development, Lambda taught me to push myself and to always be hungry for new knowledge.
                         </p>
                         <div className="flex mb-10">
                             <a href="https://github.com/ohfr" className="link-class">Github</a>
                             <a href="https://linkedin.com/in/daniel-p-martin" className="link-class">LinkedIn</a>
-                            <a href="https://gitlab.valcom.com/dmartin" className="link-class">Gitlab</a>
                         </div>
-                        <img src={require('../Assets/avatar.png')} alt="avatar" />
+                        {/* <img src={require('../Assets/avatar.png')} alt="avatar" /> */}
                     </div>
                 </div>
             </div>
