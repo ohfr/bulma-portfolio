6bd22cba011069d43bc3c4b453701bf6462352bb
Daniel <danielmartin1205@gmail.com>
2022-06-11 21:59:13 -0400

    use source code from react-typical, with link to original project, to avoid its dependency for an older react version

---
+++ src/Components/Typical.js
@@ -0,0 +1,33 @@
+// this is copied from react-typical to avoid its dependency for a lower react version  https://github.com/catalinmiron/react-typical
+import React, { useRef, useEffect, memo } from "react";
+import { type, type as loopedType } from "@camwiegert/typical";
+import styles from '../Typical.css'
+
+const Typical = ({ steps, loop, className, wrapper = "p" }) => {
+  const typicalRef = useRef(null);
+  const Component = wrapper;
+  const classNames = [styles.typicalWrapper];
+  
+  if (className) {
+    classNames.unshift(className);
+  }
+
+  useEffect(() => {
+    if (loop === Infinity) {
+      type(typicalRef.current, ...steps, loopedType);
+    } else if (typeof loop === "number") {
+      type(
+        typicalRef.current,
+        ...Array(loop)
+          .fill(steps)
+          .flat()
+      );
+    } else {
+      type(typicalRef.current, ...steps);
+    }
+  });
+
+  return <Component ref={typicalRef} className={classNames.join(' ')}/>;
+}
+
+export default memo(Typical)
